71fa085dbca1199d7883f9546627f4d2c01ab3ac
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-12 19:47:19 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,5 +3,7 @@
 
 def meal_choice(veg1, veg2, protein = "tofu")
   puts "What a nutritious meal!"
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  three_variable_sentence = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts three_variable_sentence
+  three_variable_sentence
 end
