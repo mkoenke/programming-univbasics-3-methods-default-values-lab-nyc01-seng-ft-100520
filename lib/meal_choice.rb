8834bdf1d65977e7a82821ea4fae480c51dd3c29
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-12 19:47:53 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,9 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
+
+def meal_choice(veg1, veg2, protein = "tofu")
+  puts "What a nutritious meal!"
+  three_variable_sentence = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts three_variable_sentence
+  three_variable_sentence
+end
